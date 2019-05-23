2f13c95d812a1e63964ac34b3ff5a7253bd6ecaa
SteapaSnotor <santoslucas347@gmail.com>
2019-05-23 16:16:48 -0300

    Art assets and some work on the world objects feature

--- scripts/WorldObject.gd
+++ scripts/WorldObject.gd
@@ -7,8 +7,30 @@ extends AnimatedSprite
 
 """
 
+enum TYPE{
+	CHAIR,
+	BED,
+	PICNIC,
+	CAR,
+	TELEPHONE,
+	DECORATION
+}
+
+export(TYPE) var type = TYPE.DECORATION
 export var id = 0
-export var activable = false
+export var is_activable = false
+export var is_usable = false
+
+#this means that it will cause effects (if activated) when the teen is
+#close to the object, without using it.
+export var is_detectable = false
+
+export(String) var obj_name
+export(String) var obj_desc
+
+
+var current_teen = []
+var is_broken = false
 
 #effects this object can cause when activated
 var effects = {
@@ -18,9 +40,18 @@ var effects = {
 
 #initialize
 func _ready():
-	#TODO: load json data
 	pass
 
+func _process(delta):
+	pass
+
+#trys will activate the effects of this trap
+func activate():
+	pass
+
+##Effects##
 func startle(teenager):
 	pass
-	
+
+func broke_obj():
+	pass
