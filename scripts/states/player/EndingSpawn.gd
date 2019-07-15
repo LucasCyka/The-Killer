4d872eb40291bd201911dbae57f1cda68efb7986
SteapaSnotor <santoslucas347@gmail.com>
2019-07-15 18:42:03 -0300

    Fixied a performance problem that happened when the player was spawning

--- scripts/states/player/EndingSpawn.gd
+++ scripts/states/player/EndingSpawn.gd
@@ -21,11 +21,36 @@ func init(base,state_position,state_time):
 	#search a final tile for the player to spawn
 	var tilemap = self.game.get_pathfinding_tile()
 	var floor_tiles = tilemap.get_used_cells_by_id(0)
+	var player_tile = tilemap.world_to_map(base.player.global_position)
+	
+	var player_neighbours = []
+	
+	for tile in range(8):
+		for x in range(10):
+			if floor_tiles.find(Vector2(player_tile.x+x,player_tile.y)) != -1:
+				player_neighbours.append(Vector2(player_tile.x+x,player_tile.y))
+			elif floor_tiles.find(Vector2(player_tile.x-x,player_tile.y)) != -1:
+				player_neighbours.append(Vector2(player_tile.x-x,player_tile.y))
 		
-	floor_tiles = common.convert_to_world(floor_tiles,tilemap)
+		for y in range(10):
+			if floor_tiles.find(Vector2(player_tile.x,player_tile.y+y)) != -1:
+				player_neighbours.append(Vector2(player_tile.x,player_tile.y+y))
+			elif floor_tiles.find(Vector2(player_tile.x,player_tile.y-y)) != -1:
+				player_neighbours.append(Vector2(player_tile.x,player_tile.y-y))
 		
-	final_spawn_position = common.order_by_distance(floor_tiles,base.player.global_position)[0]
+		if player_neighbours.size() >= 8:
+			break
+		
+	
+	player_neighbours = common.convert_to_world(player_neighbours,tilemap)
+	
+	final_spawn_position = common.order_by_distance(player_neighbours,base.player.global_position)[0]
 	final_spawn_position = star.get_closest_tile(final_spawn_position)
+	
+	#floor_tiles = common.convert_to_world(floor_tiles,tilemap)
+		
+	#final_spawn_position = common.order_by_distance(floor_tiles,base.player.global_position)[0]
+	#final_spawn_position = star.get_closest_tile(final_spawn_position)
 	
 	
 func update(delta):
