d3b44246f2b16b229d1a89330763a8e9fcc98800
SteapaSnotor <santoslucas347@gmail.com>
2019-07-22 17:27:21 -0300

    now the player can move the camera with the mouse

--- scripts/PlayerController.gd
+++ scripts/PlayerController.gd
@@ -16,11 +16,25 @@ const target_zoom = Vector2(0.1,0.1)
 export var max_constraints = Vector2(9999,9999)
 export var min_constraints = Vector2(-9999,-9999)
 
+#keep track os scroller at the edges of the screen being activated
+var _scroll_left = false
+var _scroll_right = false
+var _scroll_up = false
+var _scroll_down = false
+
 #init
 func _ready():
-	#TODO: set a limit for the camera movement
-	pass
-
+	#connect edge scrollers signals
+	$camera/Scrollers/AreaLeft.connect('mouse_entered',self,'update_scroller',['left',true])
+	$camera/Scrollers/AreaLeft.connect('mouse_exited',self,'update_scroller',['left',false])
+	$camera/Scrollers/AreaRight.connect('mouse_entered',self,'update_scroller',['right',true])
+	$camera/Scrollers/AreaRight.connect('mouse_exited',self,'update_scroller',['right',false])
+	$camera/Scrollers/AreaUp.connect('mouse_entered',self,'update_scroller',['up',true])
+	$camera/Scrollers/AreaUp.connect('mouse_exited',self,'update_scroller',['up',false])
+	$camera/Scrollers/AreaDown.connect('mouse_entered',self,'update_scroller',['down',true])
+	$camera/Scrollers/AreaDown.connect('mouse_exited',self,'update_scroller',['down',false])
+	
+	
 #camera movement
 func _physics_process(delta):
 	var movement = Vector2(0,0)
@@ -33,6 +47,8 @@ func _physics_process(delta):
 	movement.y = int(down) + int(up) * -1
 	move_camera_to(movement,delta)
 	
+	if !left and !right and !up and !down:
+		_move_camera_with_mouse(delta)
 
 #camera zoom-in/zoom-out
 func _input(event):
@@ -70,4 +86,25 @@ func zoom_camera(zoom):
 		#TODO: zoom smoothing
 		camera.set_zoom(camera.get_zoom() + zoom)
 		return true
+
+#will detect when the player is moving the camera by hovering the edges
+#of the screen.
+func _move_camera_with_mouse(delta):
+	var movement = Vector2(0,0)
+	
+	movement.x = int(_scroll_right) + (int(_scroll_left) * -1)
+	movement.y = int(_scroll_down) + int(_scroll_up) * -1
+	move_camera_to(movement,delta)
+
+#activate/deactivate scrollers on the edge of the screen
+func update_scroller(scroller,value):
+	match scroller:
+		'left':
+			_scroll_left = value
+		'right':
+			_scroll_right = value
+		'up':
+			_scroll_up = value
+		'down':
+			_scroll_down = value
 
